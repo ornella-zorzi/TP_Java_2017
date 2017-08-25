655db4339574f543e05a7511bed6d009f23022f5
julietach <julietachaparro94@gmail.com>
2017-08-25 18:23:30 -0300

    Update Persona.java

--- Tp_Java_2017/src/entity/Persona.java
+++ Tp_Java_2017/src/entity/Persona.java
@@ -6,9 +6,10 @@ public class Persona {
 	private String apellido;
 	private String email;
 	private String usuario;
-	private String contraseña;
+	private String contraseÃ±a;
 	private boolean habilitado;
 	private int id;
+	private int hola;
 private Categoria categoria;
 	
 	public Categoria getCategoria(){
@@ -55,11 +56,11 @@ private Categoria categoria;
 	public void setUsuario(String usuario) {
 		this.usuario = usuario;
 	}
-	public String getContraseña() {
-		return contraseña;
+	public String getContraseÃ±a() {
+		return contraseÃ±a;
 	}
-	public void setContraseña(String contraseña) {
-		this.contraseña = contraseña;
+	public void setContraseÃ±a(String contraseÃ±a) {
+		this.contraseÃ±a = contraseÃ±a;
 	}
 	public boolean isHabilitado() {
 		return habilitado;
@@ -71,13 +72,13 @@ private Categoria categoria;
 	public Persona(){
 		
 	}
-	public Persona(String dni, String nombre, String apellido,String email, String usuario, String contraseña,boolean habilitado){
+	public Persona(String dni, String nombre, String apellido,String email, String usuario, String contraseÃ±a,boolean habilitado){
 		this.setDni(dni);
 		this.setNombre(nombre);
 		this.setApellido(apellido);
 		this.setEmail(email);
 		this.setUsuario(usuario);
-		this.setContraseña(contraseña);
+		this.setContraseÃ±a(contraseÃ±a);
 		this.setHabilitado(habilitado);
 			
 	}
